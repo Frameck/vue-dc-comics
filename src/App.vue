f3d5394b751c399a997a90aa24c7ab5df9feea38
Frameck <fra9879@gmail.com>
2021-11-11 14:51:38 +0100

    Completed header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -25,4 +24,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+@import 'styles/variables';
+@import 'styles/main';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="header-container">
+      <header class="main-header">
+          <img class="logo" src="../assets/dc-logo.png" alt="dc-logo">
+          <ul>
+            <li v-for="item,i in navbarItems" :key="i">
+              <a 
+              :href="item.href + item.text"
+              :class="i === currentActive ? 'active' : ''">
+                {{ item.text.toUpperCase() }}
+              </a>
+            </li>
+          </ul>
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      navbarItems: [
+        {
+          text: 'characters',
+          href: '#'
+        },
+        {
+          text: 'comics',
+          href: '#'
+        },
+        {
+          text: 'movies',
+          href: '#'
+        },
+        {
+          text: 'tv',
+          href: '#'
+        },
+        {
+          text: 'games',
+          href: '#'
+        },
+        {
+          text: 'collectibles',
+          href: '#'
+        },
+        {
+          text: 'videos',
+          href: '#'
+        },
+        {
+          text: 'fans',
+          href: '#'
+        },
+        {
+          text: 'news',
+          href: '#'
+        },
+        {
+          text: 'shop',
+          href: '#'
+        }
+      ],
+      currentActive: 1
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
